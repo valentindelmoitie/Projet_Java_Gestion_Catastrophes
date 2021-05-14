4cbd83f14ad668fc7c32d665257fe3454c03fbc1
KucMa <82814479+KucMa@users.noreply.github.com>
2021-05-14 15:51:59 +0200

    Ajout test unitaire sur BusinessTaskManager

---
+++ src/test/BusinessTaskManagerTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import business.BusinessTaskManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessTaskManagerTest {
+    BusinessTaskManager businessTaskManager;
+
+    @BeforeEach
+    public void setUp(){
+        businessTaskManager = new BusinessTaskManager();
+    }
+
+    @Test
+    void calculatePourc() {
+        assertEquals(50.0,businessTaskManager.calculatePourc(1000,500),0.01);
+    }
+
+    @Test
+    void calculatePourcDividedByZero() {
+        assertEquals(0.0,businessTaskManager.calculatePourc(0,1248584),0.01);
+    }
+}
